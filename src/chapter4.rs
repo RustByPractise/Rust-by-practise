b6a2bcbe68ba46b126272da1dcae130dec67845a
RustByPractise <vanya2006bio@gmail.com>
2024-10-10 13:36:53 +0300

    Update chapter4.rs

--- src/chapter4.rs
+++ src/chapter4.rs
@@ -211,3 +211,73 @@ fn sum(x: i32, y: i32) -> i32 {
     x + y 
 }
 
+4.4. Functions
+
+fn main() {
+    let (x, y) = (1, 2);
+    let s = sum(x, y);
+
+    assert_eq!(s, 3);
+
+    println!("Success!");
+}
+
+fn sum(x: i32, y: i32) -> i32 { 
+    x + y 
+
+    fn main() {
+   print();
+}
+fn print() {
+   println!("Success!");
+}
+
+
+fn main() {
+    never_return();
+
+    println!("Failed!");
+}
+
+fn never_return() -> ! {
+    panic!("This function never returns!"); 
+}
+
+
+fn main() {
+    println!("Success!");
+}
+
+fn get_option(tp: u8) -> Option<i32> {
+    match tp {
+        1 => {
+            Some(42) 
+        }
+        _ => {
+            None 
+        }
+    };
+    
+    
+    never_return_fn()
+}
+
+fn never_return_fn() -> ! {
+    std::process::exit(1); 
+}
+
+
+fn main() {
+    // FILL in the blank
+    let b = false; 
+
+    let _v = match b {
+        true => 1,
+        false => {
+            println!("Success!");
+            panic!("we have no value for `false`, but we can panic");
+        }
+    };
+
+    println!("Exercise Failed if printing out this line!");
+}
